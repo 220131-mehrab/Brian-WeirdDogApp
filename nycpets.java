f885f9d99631214244b043adaf2247cad760871a
[Brian <brian.w.727@gmail.com>
2022-02-08 13:54:20 -0700

    add csv

--- nycpets.java
+++ nycpets.java
@@ -1,6 +1,20 @@
-class nycPets {
-	public static void main(String[] args) {
-		System.out.println("Welcome to NYC Pets");
+import java.io.File;
+import java.util.Scanner;
 
+
+class nycPets {
+	public static void main(String[] args) throws Exception {
+		if (args.length < 1) {
+			System.out.println("\nError: No filename provided.");
+			System.out.println("\tUSAGE: java nycPets <filename>");
+		} else {
+			String filename = args[0];
+			//System.out.println("Num of args: " + args.length);
+			File nycdogs = new File(filename);
+			Scanner sc = new Scanner(nycdogs);
+			while (sc.hasNext()) {
+				System.out.println(sc.next());
+			}
+		}
 	}
 }
